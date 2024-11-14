6deb3474a5bf3fa85686a6d39474f69f03c0d6bc
Andrey Nikonenko <sonanted@gmail.com>
2024-11-14 17:16:20 +0300

    feat: cards list render & card deletion

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,22 @@
-// @todo: Темплейт карточки
+const cardTemplate = document.querySelector("#card-template").content;
+const cards = document.querySelector(".places__list");
 
-// @todo: DOM узлы
+function createCard(props, deleteFunction) {
+  const card = cardTemplate.querySelector(".card").cloneNode(true);
+  card.querySelector(".card__title").textContent = props.name;
+  card.querySelector(".card__image").src = props.link;
+  card
+    .querySelector(".card__delete-button")
+    .addEventListener("click", deleteFunction);
+  return card;
+}
 
-// @todo: Функция создания карточки
+function deleteCard(event) {
+  event.target.parentNode.remove();
+}
 
-// @todo: Функция удаления карточки
+function renderCards() {
+  initialCards.forEach((item) => cards.append(createCard(item, deleteCard)));
+}
 
-// @todo: Вывести карточки на страницу
+renderCards();
